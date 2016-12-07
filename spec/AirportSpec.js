d8e6dccbc165a5223854380e5b271db33b78bb0e
malin patel <malin.patel@icloud.com>
2016-12-07 12:57:06 +0000

    Added remaining tests

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,16 +4,22 @@ describe("Airport", function(){
 
   var airport;
   var plane;
+  var weather;
 
   beforeEach(function(){
-    airport = new Airport();
     plane = jasmine.createSpy('plane', ['land']);
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    airport = new Airport(weather);
   });
 
   it ("has no planes by default", function(){
     expect(airport.planes()).toEqual([]);
   });
 
+  describe('under normal conditions', function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    });
   it ("can clear planes for landing", function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
@@ -24,9 +30,17 @@ describe("Airport", function(){
     airport.clearForTakeOff(plane);
     expect(airport.planes()).toEqual([]);
   });
-
-  it ("can check for stormy conditions",function(){
-    expect(airport.isStormy()).toBeFalsy();
+});
+describe('under stormy conditions',function(){
+  beforeEach(function() {
+    weather.isStormy.and.returnValue(true);
   });
 
+  it('does not clear planes for landing', function() {
+    expect(function() {airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
+  });
+    it('does not clear planes for takeoff', function(){
+      expect(function() {airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+    });
+  });
 });
